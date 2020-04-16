ae643f6d42fcc940a7e74417d361437b2714cf95
REBECCA_FORTES <RMFORTES@UMICH.EDU>
2020-04-15 23:47:13 -0400

    all routes built

--- index.js
+++ index.js
@@ -7,20 +7,21 @@ const app = express();
 app.use(parser.json());
 
 
-//? this route lists every object
+// this route lists every object
 app.get("/list", (req, res) => {
   Word.find({}).then((words) => {
     res.json(words);
   });
 });
 
-//this route lists a single object 
+//this route lists a single object by id search
 app.get('/list/:id', (req, res) => {
     Word.findById(req.params.id).then(words => {
         res.json(words);
     })
 })
 
+//this route lists a single object by word search
 app.get('/list/word/:word', (req, res) => {
     Word.find({word: req.params.word}).then(words=>{
         res.json(words);
@@ -28,15 +29,30 @@ app.get('/list/word/:word', (req, res) => {
 })
 
 //this route creates a new object 
-// app.post("/", (req, res) => {
-//   Word.find({}).then((words) => {
-//     res.json(words);
-//   });
-// });
+app.post("/list/", (req, res) => {
+  Word.create(req.body).then(words=>{
+      res.json(words);
+  });
+});
 
 //this route updates an object 
+app.put("/list/:id", function (req, res) {
+    Word.findOneAndUpdate(
+      { id: req.params.id },
+      req.body,
+      { new: true }
+    ).then(words => {
+      res.json(words)
+    })
+  })
 
 //this route deletes an object. 
+app.delete("/list/:id", function (req, res) {
+    Word.findOneAndRemove({ _id: req.params.id })
+      .then(words => {
+        res.json(words)
+      })
+  })
 
 app.listen(4000, () => {
   console.log("Listening on 4000!");
